483b4c57bc1fbdae50f7a32d387bd41a422960ae
AideenM12 <a.malone1289@gmail.com>
2021-05-10 16:37:11 +0000

    add form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,15 @@
-/*
-    jQuery for MaterializeCSS initialization
-*/
-
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
 
 
@@ -17,12 +23,17 @@ $(document).ready(function () {
 // document.addEventListener('DOMContentLoaded', function () {
 //     let sidenavs = document.querySelectorAll(".sidenav");
 //     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+//     let tooltips = document.querySelectorAll(".tooltipped");
+//     let tooltipsInstance = M.Tooltip.init(tooltips);
+//     let datepickers = document.querySelectorAll(".datepicker");
+//     let datepickersInstance = M.Datepicker.init(datepickers, {
+//         format: "dd mmmm, yyyy",
+//         yearRange: 3,
+//         showClearBtn: true,
+//         i18n: {
+//             done: "Select"
+//         }
+//     });
 // });
-
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-})
-
-$(document).ready(function(){
-    $('.tooltipped').tooltip();
-  });
